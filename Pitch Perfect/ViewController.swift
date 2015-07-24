81bb9357b81fdaea051f0832597a138a0204f475
Dany Cabrera Vargas <danyalejandro@MacBook-Pro-de-Dany.local>
2015-07-24 14:21:39 -0500

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Dany Cabrera Vargas on 24/07/15.
+//  Copyright (c) 2015 Dany Alejandro Cabrera Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
